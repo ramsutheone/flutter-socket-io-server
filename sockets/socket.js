937bd0139be6b1f3c30f3ae7c26b9e39e5338105
Raul Moretti <raulmoretti@gmail.com>
2021-04-20 15:46:36 -0300

    Fin creacion basica

---
+++ sockets/socket.js
@@ -0,0 +1,15 @@
+const { io } = require('../index');
+
+//Mensajes de sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+    client.on('disconnect', () => {
+        console.log('Cliente Desonectado');
+    });
+
+    client.on('mensaje', (payload) => {
+        console.log('Mensaje', payload);
+
+        io.emit('mensaje', { admin: 'Nuevo Mensaje' });
+    });
+});
